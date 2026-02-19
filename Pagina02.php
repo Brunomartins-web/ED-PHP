4758de3509211d52e48075962873362d0fb68c9c
BRUNO DE ARAUJO MARTINS <bruno.amartins4@senacsp.edu.br>
2026-02-18 21:37:07 -0300

    Add-pagina02.php

---
+++ Pagina02.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    //criando variavel
+    $nomeCompleto = "Alguém";
+?>
+    <h2>Nome completo: <?php echo $nomeCompleto; ?></h2>
+
+</body>
+</html>
